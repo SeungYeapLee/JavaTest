164c62227478fa1587521b727ca333b3dd2217e4
Simon Kang <xeungjin@nate.com>
2018-08-24 20:39:11 +0900

    Create enumTest.java

---
+++ enumTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class enumTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			int inp, cnt = 0;
+			do {
+				inp = scanner.nextInt();
+				if (inp == 0)
+					break;
+				if (!((inp % 3 == 0) || (inp % 5 == 0))) {
+					cnt++;
+				}
+			} while (true);
+			System.out.println(cnt);
+		} finally {
+			if (scanner != null)
+				scanner.close();
+		}
+	}
+}
